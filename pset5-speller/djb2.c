6c1405532ecabd56017913162e3e9e1b6281f65c
B S Anandu <59107214+BSanandu88@users.noreply.github.com>
2020-06-03 17:24:53 +0530

    Add files via upload

---
+++ pset5-speller/djb2.c
@@ -0,0 +1,15 @@
+#include "djb2.h"
+
+unsigned long djb2(const void *_str)
+{
+  const char *str = _str;
+  unsigned long hash = 5381;
+  int c;
+
+  while ((c = *str++))
+  {
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  }
+
+  return hash;
+}
